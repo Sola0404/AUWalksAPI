0fbf6e5fc28ac709e87c1913a5cf85879f986a0f
Yujia <0404sola@gmail.com>
2024-08-27 12:55:47 +1000

    Create login action method

--- AUWalksAPI/Controllers/AuthController.cs
+++ AUWalksAPI/Controllers/AuthController.cs
@@ -42,5 +42,27 @@ namespace AUWalksAPI.Controllers
 
             return BadRequest("Something went wrong.");
         }
+
+        // POST: /api/auth/login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginRequestDto.Username);
+
+            if (user != null)
+            {
+                var checkPasswordResult = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                if (checkPasswordResult)
+                {
+                    // Create token
+
+                    return Ok();
+                }
+            }
+
+            return BadRequest("Username or password is incorrect.");
+        }
     }
 }
